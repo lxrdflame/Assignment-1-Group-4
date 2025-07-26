c49d8b115b3cedf0f1f91e64159d222eb6c619f9
lxrdflame <tyrique.s.r@gmail.com>
2025-07-26 22:04:36 +0200

    damage indicator Update and level design

---
+++ Assignment1Group4GD/Assets/Scripts/DamageIndicator.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageIndicator : MonoBehaviour
+{
+
+    void Update()
+    {
+      transform.Translate(Vector3.up * 2.5f * Time.deltaTime);
+    }
+}
