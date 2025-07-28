6ef17dc5415ac09a9d634cce62f55611bc4123ef
lxrdflame <tyrique.s.r@gmail.com>
2025-07-28 16:13:45 +0200

    Leve Design Changes

--- Assignment1Group4GD/Assets/Scripts/Enemy Script.cs
+++ Assignment1Group4GD/Assets/Scripts/Enemy Script.cs
@@ -5,5 +5,23 @@ using UnityEngine;
 public class EnemyScript : MonoBehaviour
 {
     public int HP;
+    public GameObject Player;
 
+    private void Start()
+    {
+        Player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    private void Update()
+    {
+        float distance = Vector3.Distance(transform.position, Player.transform.position);
+        if (distance <= 10)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, Player.transform.position, 3 * Time.deltaTime);
+        }
+        if (HP <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
 }
