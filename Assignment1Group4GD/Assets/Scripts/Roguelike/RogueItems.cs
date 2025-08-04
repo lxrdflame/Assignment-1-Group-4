01fd6b693ff10e4b0fa2ebe46e078a307f725164
bigchungus <shivolong4@gmail.com>
2025-08-04 21:27:33 +0200

    Added Perk system
    if something goes wrong edit itempurchase and rogueitems also edit some of the scriptable objects

--- Assignment1Group4GD/Assets/Scripts/Roguelike/RogueItems.cs
+++ Assignment1Group4GD/Assets/Scripts/Roguelike/RogueItems.cs
@@ -4,7 +4,25 @@ using UnityEngine;
 public class Rogueitems : ScriptableObject
 {
     public string itemName;
-     public string description;
+    public string description;
     public GameObject itemPrefab;
     public Sprite itemSprite;
+    
+    [Header("Stat Upgrade Info")]
+    public bool modifiesStats = false;
+
+    public enum StatType
+    {
+        None,
+        Damage,
+        FireRate,
+        MoveSpeed,
+        JumpHeight,
+        DescentSpeed,
+        MaxHealth
+    }
+
+    public StatType statToModify = StatType.None;
+    public float statValue = 0f;
+
 }
